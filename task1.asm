5b3f8c380b0f663450acad80e3bdcca46b60d54b
Dillon Reading <dread@dread-arch.mypc.usu.edu>
2025-10-07 14:46:39 -0600

    task 1 done

--- task1.asm
+++ task1.asm
@@ -1,13 +1,30 @@
-# Hello World in MIPS
 .data
-message: .asciiz "Hello, World!\n"
+a: .word 4
+b: .word 7
+c: .word 10
+result: .word 0
 
 .text
 .globl main
-main:
-    li $v0, 4          # syscall code for print_string
-    la $a0, message    # load address of the string
-    syscall
 
-    li $v0, 10         # syscall code for exit
-    syscall
+main:
+# load variables into registers
+lw $t0, a
+lw $t1, b
+lw $t2, c
+
+# do addition
+add $t3, $t0, $t1
+add $t3, $t3, $t2
+
+#store result
+sw $t3, result
+
+#print result and newline
+li $v0, 1
+move $a0, $t3
+syscall
+li $v0, 11
+li $a0, 10
+syscall
+ 
